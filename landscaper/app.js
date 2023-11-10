0ba43300f746f85e0895c59c6b947eb85e5304d9
Hojoon Kim <hojoon0724@gmail.com>
2023-11-10 15:54:57 -0800

    set up money dynamics for cut lawns with teeth

--- landscaper/app.js
+++ landscaper/app.js
@@ -1,3 +1,35 @@
 console.log("noop noop");
 // start
-// cut lawns = $1
+const landscaper = {
+  name: "userName",
+  money: 0,
+  tools: [],
+};
+console.log(landscaper.name);
+//landscaper.name = prompt("What's your name?");
+console.log(landscaper.name);
+console.log(landscaper.tools);
+// cut lawns with teeth = +1
+const cutWithTeeth = () => {
+  landscaper.money += 1;
+};
+console.log(landscaper.money);
+
+// rusty scissors = -5
+
+// cut lawns with rusty scissors = +5
+
+// old-timey push lawnmower = -25
+
+// cut lawns with old-timey push lawnmower = +50
+
+// buy fancy battery-powered lawnmower = -250
+
+// cut lawns with battery-powered lawnmower = $100
+
+// hire a team of starving students = -500
+
+// cut lawns with a team of starving students = +250
+
+// win the game when you have a team of starving students and 1000
+// say that they won the game
