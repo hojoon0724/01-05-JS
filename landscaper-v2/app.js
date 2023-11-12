527b7e50769434a6586fbfaddeb8e90c0b1018b6
Hojoon Kim <hojoon0724@gmail.com>
2023-11-11 16:19:10 -0800

    Starting another v2 of landscaper

---
+++ landscaper-v2/app.js
@@ -0,0 +1,14 @@
+const tools = [
+  { level: 0, name: "Teeth", cost: 0, generates: 1 },
+  { level: 1, name: "Rusty Scissors", cost: 5, generates: 5 },
+  { level: 2, name: "Old-timey Push Lawnmower", cost: 25, generates: 50 },
+  { level: 3, name: "Fancy Battery-Powered Lawnmower", cost: 250, generates: 100 },
+  { level: 4, name: "Team of Starving Students", cost: 500, generates: 250 },
+  { level: 5, name: "No more upgrades", cost: 20000, generates: 250 },
+];
+
+const landscaper = {
+  name: "",
+  money: 0,
+  toolbox: "",
+};
